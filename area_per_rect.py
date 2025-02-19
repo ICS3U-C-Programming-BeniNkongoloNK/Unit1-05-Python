b49d26cd8258877c04a9286cc9c45800b177641c
benink <117493347+benink@users.noreply.github.com>
2025-02-19 17:31:17 +0000

    Added area_per_rect.py file

---
+++ area_per_rect.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# Created By: Beni
+# Date: February 18, 2025
+# Calculates the area of a rectangle.
+
+
+def main():
+    # calculates area and perimeter
+    print("If a rectangle has the dimensions:")
+    print("4cm * 3cm")
+    print("")
+    print("Area is{}cm^2".format(4 * 3))
+
+
+if __name__ == "__main__":
+    main()
